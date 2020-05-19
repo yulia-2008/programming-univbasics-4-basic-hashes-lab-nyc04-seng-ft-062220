51fb1c77511700c78899632cd806562a70fc9cc8
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-19 17:56:17 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,18 @@
 def new_hash
-  # return an empty hash
+  new=Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ new={name: "Yulia", last_name: "Yurkevich"}
 end
 
 def pioneer
+  new_hash={:name => "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  new={:id => number}
   # return a hash with a key :id assigned to the provided number
 end
